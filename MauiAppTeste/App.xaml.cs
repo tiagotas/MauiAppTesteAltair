b36ceae13728c8cce4658f00ea5fefad8a92b936
Tiago <frost.tiago@gmail.com>
2024-02-22 14:47:22 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppTeste/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppTeste
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
